1bf56287c797afe586a83ad2a4b19fd34cb43d9a
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-08 17:08:07 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit3-04-HTML-PWA
+// April 8 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit3-04-HTML-PWA/sw.js", {
+    scope: "/ICS2O-Unit3-04-HTML-PWA/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Calculates fahrenheit to celsius and celsius to fahrenheit using the user's input. 
  */
-function onButtonClick() {
-  alert("Hello, World!")
+function calculate(input) {
+  if (input == 'fahrenheit') { // If the input is fahrenheit, do fahrenheit to celsius.
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+    const answer = ((fahrenheit - 32) * (5/9)).toFixed(2)
+    document.getElementById("output-celsius").innerHTML = "<p>This temperature is equal to " + answer + "°C</p>"
+  } else if (input == 'celsius') { // If the input is celsius, do celsius to fahrenheit.
+    const celsius = parseFloat(document.getElementById("celsius").value)
+    const answer = ((celsius * (9/5)) + 32).toFixed(2)
+    document.getElementById("output-fahrenheit").innerHTML = "<p>This temperature is equal to " + answer + "°F</p>"
+  }
 }
